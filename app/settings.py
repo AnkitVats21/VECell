dfe9ff2fb84afc2cea4f7e082ca67a4efabbb3d4
AnkitVats21 <ankvats21@gmail.com>
2022-01-26 17:05:28 +0530

    database added settings updated

--- app/settings.py
+++ app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'njjv9z_jj$*9py23@g5u*(nkquqky3z!mgzs5cn#mmfikln1!+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
